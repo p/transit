2fa75cbf4901dc756bb1b07982416e58ab3dcb13
Oleg Pudeyev <oleg@bsdpower.com>
2020-01-29 02:38:37 -0500

    api fetch

--- frontend/src/actions/path-times-actions.js
+++ frontend/src/actions/path-times-actions.js
@@ -1,4 +1,20 @@
 import * as u from '../util'
 import reactor from '../reactor'
+import _ from 'underscore'
 
-export function fetch_path_times() {}
+const STATIONS=[
+'thirty_third_street','world_trade_center','journal_square']
+
+const PATH_API_URL='https://path.api.razza.dev/v1'
+
+export function fetch_path_times() {
+        fetch(`${PATH_API_URL}/stations`)
+        .then(resp=>resp.json())
+    .then(payload=>reactor.dispatch('receive_stations',payload))
+    
+    _.each(STATIONS,station=>{
+        fetch(`${PATH_API_URL}/stations/${station}/realtime`)
+        .then(resp=>resp.json())
+        .then(payload=>reactor.dispatch('receive_station_realtime',payload))
+    })
+    }
