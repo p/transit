889af585f3d4fdf35bbcfbaa2137b12c629fa94a
Oleg Pudeyev <oleg@bsdpower.com>
2020-02-02 18:54:36 -0500

    mta note

---
+++ frontend/src/stores/mta-store.js
@@ -0,0 +1,5 @@
+// Data url:
+// https://collector-otp-prod.camsys-apps.com/realtime/serviceStatus?apikey=qeqy84JE7hUKfaI0Lxm2Ttcm6ZA0bYrP
+//
+// api key is included in:
+// https://new.mta.info/themes/custom/bootstrap_mta/js/apps/service-status.js
